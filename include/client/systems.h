c796fa57f7abd82ad9107212f8bd8d390e5d5c4b
gab462 <62474485+gab462@users.noreply.github.com>
2026-01-04 11:42:28 -0300

    Fix gitignore and add systems.h

---
+++ include/client/systems.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <flecs.h>
+
+void RenderModel(ecs_iter_t* it);
+
+void Move(ecs_iter_t* it);
+
+void Animate(ecs_iter_t* it);
+
+void MoveCamera(ecs_iter_t* it);
+
+void KeyboardControls(ecs_iter_t* it);
